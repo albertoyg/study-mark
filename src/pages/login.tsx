a4b76bfdf9304dac85b14a9638722fa5436130ac
Alberto Yanes <albertoyanesgca@gmail.com>
2023-01-24 12:01:15 -0800

    I made a change

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { DispatchWithoutAction, useEffect, useState } from "react";
 
-// Hello
+// I made another change
 const LoginPage = () => {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
